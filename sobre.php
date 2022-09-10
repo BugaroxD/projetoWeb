f50d30fa2555706c9c1d79bbab9a1711e616fcef
Jefferson <jeffeluiz97@gmail.com>
2022-09-10 06:39:09 -0300

    feat: button classes

--- sobre.php
+++ sobre.php
@@ -11,6 +11,13 @@ include "header.php";
             <p> O caos se espalhou pelos continentes e os dominantes poderosos caíram dos seus respectivos tronos.</p>
             <p> Com o mundo totalmente dividido, os heróis de todos os tipos irão emergir com o mesmo objetivo – ampliar os seus poderes e trazer paz para a humanidade mais uma vez!</p>
             <p> No entanto, apenas uma única alma é destinada para guiar as pessoas para fora da escuridão e por um fim nesta guerra brutal de uma vez por todas. Será você este líder da próxima geração?</p>
+            <h1>
+                <nav class="main">
+                    <ul class="container">
+                        <li><a href="classes.php">Classes</a></li>
+                    </ul>
+                </nav>
+            </h1>
         </div>
         <div class="col-6">
             <h1>Reborn Fighters</h1>
