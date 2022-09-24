a1927a6df2b89208e14572cac1b8277e6d37cbaa
BugaroxD <jeffeluiz97@gmail.com>
2022-09-23 21:44:04 -0300

    chore: recovery att

--- index.php
+++ index.php
@@ -9,8 +9,7 @@ include "header.php";
             <h1>Bem vindo</h1>
             <p>Reborn Fighters.</p>
             <p>O melhor jogo de luta que você verá, com uma gama imensa de classes e gears.</p>
-        </div>
-        <div class="col col-sm-12 col-md-12">
+            s
             <h1>Video</h1>
             <iframe width="660" height="315" src="https://www.youtube.com/embed/ZNBJZ3Shj2o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
